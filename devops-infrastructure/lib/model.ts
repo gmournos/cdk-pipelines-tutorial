6c5ffcd32c429720826e8691adb29cc224998725
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-10-31 05:27:57 +0100

    codeartifact repo for the tutorial

--- devops-infrastructure/lib/model.ts
+++ devops-infrastructure/lib/model.ts
@@ -1,8 +1,11 @@
-
 export const Accounts = {
     DEVOPS: process.env.DEVOPS_ACCOUNT || 'default-devops-account',
-    DEVELOPMENT: process.env.DEVELOPMENT_ACCOUNT || 'default-development-account', 
+    DEVELOPMENT: process.env.DEVELOPMENT_ACCOUNT || 'default-development-account',
     TEST: process.env.TEST_ACCOUNT || 'default-test-account',
     ACCEPTANCE: process.env.ACCEPTANCE_ACCOUNT || 'default-acceptance-account',
     PRODUCTION: process.env.PRODUCTION_ACCOUNT || 'default-production-account',
-  } as const; // immutable
+} as const; // immutable
+
+export const DOMAIN_NAME = 'cdk-pipelines-tutorial-artifact-domain';
+export const NPM_REPO = 'cdk-pipelines-tutorial-npm-repo';
+export const COMMON_REPO = 'cdk-pipelines-tutorial-common-repo';
