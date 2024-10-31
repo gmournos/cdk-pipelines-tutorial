718d8b3ce88250ae3b6c918105be53f6f4fd3bca
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-10-31 06:23:43 +0100

    9 source code bucket

--- devops-infrastructure/lib/model.ts
+++ devops-infrastructure/lib/model.ts
@@ -9,3 +9,9 @@ export const Accounts = {
 export const DOMAIN_NAME = 'cdk-pipelines-tutorial-artifact-domain';
 export const NPM_REPO = 'cdk-pipelines-tutorial-npm-repo';
 export const COMMON_REPO = 'cdk-pipelines-tutorial-common-repo';
+export const SOURCE_CODE_BUCKET_NAME = 'cdk-pipelines-tutorial-sources-bucket';
+
+export enum StackExports {
+    PIPELINE_SOURCE_BUCKET_ARN_REF = 'uniform-pipeline-source-bucket-arn-ref',
+};
+  
