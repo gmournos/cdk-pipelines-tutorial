7322da383a72c6c6a62cf19e0ce5ad155155e0d6
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-10-31 13:36:53 +0100

    15 export the pipeline

--- complex-stack-sample/bin/complex-stack-sample.ts
+++ complex-stack-sample/bin/complex-stack-sample.ts
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { ComplexStackSampleStack } from '../lib/complex-stack-sample-stack';
+import { PipelineStack } from '../lib/pipeline-construct';
 
 const app = new cdk.App();
-new ComplexStackSampleStack(app, 'ComplexStackSampleStack', {});
+
+const inPipelines = app.node.tryGetContext('pipeline');
+const description = 'Sample Stack with many assets';
+
+if (inPipelines === 'true') {
+    new PipelineStack(app, 'pipeline-stack', {
+        containedStackName: 'ComplexStackSampleStack',
+        containedStackProps: {
+            description,
+        },
+        description: 'Pipeline stack to deploy Feature1',
+        stackName: 'PipelineStack',
+    });
+} else {
+    new ComplexStackSampleStack(app, 'ComplexStackSampleStack', {
+        description,
+    });
+}
