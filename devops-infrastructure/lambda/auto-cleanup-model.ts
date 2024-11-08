4d611b2247e25e2ba444782484507ea3f34242e4
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-08 10:49:43 +0100

    83 step function orchestrator

--- devops-infrastructure/lambda/auto-cleanup-model.ts
+++ devops-infrastructure/lambda/auto-cleanup-model.ts
@@ -1,6 +1,9 @@
 export const HISTORY_MONTHS_LENGTH = 3;
 export const MAX_HISTORY_LENGTH = 7;
 export const DELETE_BATCH_SIZE = 5;
+export const MINUTES_BETWEEN_DELETES = 3;
+export const DELETE_PROCESS_TIMEOUT_MINUTES = 60; // 1 hour
+
 
 export interface PipelineStackPair {
     pipelineName: string,
